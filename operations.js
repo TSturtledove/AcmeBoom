6cf95619444faffdf9cd9661339953a026ed8232
TSturtledove <roberttortorelli@yahoo.com>
2017-01-05 08:59:12 -0600

    made main display work but it has a bug and displays an undefind at beginning of list, added boxes in css to help separate divs

--- operations.js
+++ operations.js
@@ -47,15 +47,28 @@ Promise.all([engage01, engage02, engage03])
 
 
 function populate() {
-  console.log(details)
+  // console.log(details)
   for(var t=0; t<details.length; t++){
     for (var prop in details[t]) {
       if( details[t].hasOwnProperty( prop ) ) {
         var info = details[t][prop]
         //This line shows only shrubs
         if(info.type_id === 0 || info.type_id === 1 || info.type_id === 2){
+          //finds the correct Type
           var kind = info.type_id
+          //finds the correct category
           var catNum = types[kind].category_id
+          var list
+          list += `<div class="standDiv"><h4>${categories[catNum].name} ${types[kind].name}</h4>
+                                <h3>${info.name}</h3>
+                                <p>${info.description}</p>
+                                </div>`
+          $("#printHere").html(list)
+
+          // $("#printHere").html(`<div class=""><h4>${categories[catNum].name} ${types[kind].name}</h4>
+          //                       <h3>${info.name}</h3>
+          //                       <p>${info.description}</p>
+          //                       </div>`)
           console.log("does this work " + prop + " = " + info.name + "and it's also a " + types[kind].name + "in the " + categories[catNum].name + " category" );
           console.log(info.type_id);
         }
